dc46a51427fe4fe446121fdbdd16c9cdd7936a8c
Hemmo Karja <hemmo.karja@sok.fi>
2024-09-16 18:37:14 +0300

    Wrap prompt customization to function

--- terraform/scripts/bootstrap.sh
+++ terraform/scripts/bootstrap.sh
@@ -106,19 +106,22 @@ install_docker() {
   echo "Docker installed successfully!"
 }
 
+update_bash_prompt() {
+    cat << 'EOF' >> $HOME/.bashrc
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+export PS1="\e[35m\u@\h (dev) \[\e[36m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\] \n> "
+EOF
+  echo "Bash prompt customized!"
+}
+
 
 wait_for_network_connectivity
 update_system
 install_pyenv
 install_poetry
 install_docker
-
-# update prompt
-cat << 'EOF' >> $HOME/.bashrc
-parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-export PS1="\e[35m\u@\h (dev) \[\e[36m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\] \n> "
-EOF
+update_bash_prompt
 
 exit 0
