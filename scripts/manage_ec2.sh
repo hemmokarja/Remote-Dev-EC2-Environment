d14aebc29ca24efb68b9084580251f0a5366b991
Hemmo Karja <hemmo.karja@sok.fi>
2024-09-17 21:21:58 +0300

    Remove duplicate code

--- scripts/manage_ec2.sh
+++ scripts/manage_ec2.sh
@@ -2,12 +2,6 @@
 
 source config.sh
 
-if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set as environment" \
-    "variables! Exiting." >&2
-  exit 1
-fi
-
 # fetch ids of dev instance and bastion host in one call
 INSTANCE_IDS=$(aws ec2 describe-instances \
   --filters "Name=tag:User,Values=\"$USERNAME\"" \
