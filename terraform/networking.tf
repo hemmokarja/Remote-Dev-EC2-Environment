7102c264bd7bacb29a6714a2bbfcf50513c51a86
Hemmo Karja <hemmo.karja@sok.fi>
2024-09-18 09:06:42 +0300

    Rename resources

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-resource "aws_internet_gateway" "dev_igw" {
+resource "aws_internet_gateway" "internet_gw" {
   vpc_id = aws_vpc.dev_vpc.id
 
   tags = {
@@ -27,12 +27,12 @@ resource "aws_internet_gateway" "dev_igw" {
   }
 }
 
-resource "aws_route_table" "dev_route_table" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.dev_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dev_igw.id
+    gateway_id = aws_internet_gateway.internet_gw.id
   }
 
   tags = {
@@ -41,9 +41,9 @@ resource "aws_route_table" "dev_route_table" {
   }
 }
 
-resource "aws_route_table_association" "dev_route_table_association" {
+resource "aws_route_table_association" "public_route_table_association" {
   subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.dev_route_table.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_subnet" "private_subnet" {
